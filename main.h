76b6f954d854222cfc1adc8cba767775cb5d5a5b
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-03 11:35:29 -0500

    Add sys/types.h and sys/wait.h headers

--- main.h
+++ main.h
@@ -6,10 +6,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void prompter(void);
 void printer(const char *msg);
-void input_text(char *prompt, size_t extent);
+char **input_text(char *prompt, size_t extent);
 void snprinter(char *str, size_t extent, const char *format, ...);
+void executor(const char **arrstore);
 
 #endif /*MAIN_H_*/
