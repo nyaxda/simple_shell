646f258c9b95c262c76994d44a0dac816b36ff24
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-03 11:34:44 -0500

    Add executor function to execute commands

---
+++ executor.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void executor(const char **arrstore)
+{
+    int status;
+    pid_t child_process_id = fork();
+
+    if (child_process_id == -1) /* child process has failed to initiate*/
+    {
+        perror ("Error:");
+        exit(1);
+    }
+    else if (child_process_id == 0)
+    {
+        /*child process occurs here*/
+        if (execve (arrstore[0], arrstore, NULL) == -1)
+            perror ("Error:");
+    }
+    else
+        wait(&status);
+    snprinter(prompt_path, sizeof(prompt_path),"/bin/%s", prompt);
+}
