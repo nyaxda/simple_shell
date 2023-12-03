f923fb01654730c3aafd444479a66ebe0f7622c3
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-03 11:36:17 -0500

    Add prompter function to display the prompt symbol

--- prompter.c
+++ prompter.c
@@ -1,4 +1,11 @@
 #include "main.h"
+
+/**
+ * prompter - displays the prompt symbol ($) to the user.
+ * Description: prints the prompt symbol, indicating that the user needs
+ * to enter a command in the shell.
+ * Return: void.
+ */
 void prompter(void)
 {
     printer("($) ");
