f2a9356eb651291f0ff893162f862f4521229d97
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-03 11:43:01 -0500

    Betty-style correction on executor.c

--- executor.c
+++ executor.c
@@ -2,21 +2,21 @@
 
 void executor(const char **arrstore)
 {
-    int status;
-    pid_t child_process_id = fork();
+	int status;
+	pid_t child_process_id = fork();
 
-    if (child_process_id == -1) /* child process has failed to initiate*/
-    {
-        perror ("Error:");
-        exit(1);
-    }
-    else if (child_process_id == 0)
-    {
-        /*child process occurs here*/
-        if (execve (arrstore[0], arrstore, NULL) == -1)
-            perror ("Error:");
-    }
-    else
-        wait(&status);
-    snfprinter(prompt_path, sizeof(prompt_path),"/bin/%s", prompt);
+	if (child_process_id == -1) /* child process has failed to initiate*/
+	{
+		perror ("Error:");
+		exit(1);
+	}
+	else if (child_process_id == 0)
+	{
+		/*child process occurs here*/
+		if (execve (arrstore[0], arrstore, NULL) == -1)
+			perror ("Error:");
+	}
+	else
+		wait(&status);
+	snfprinter(prompt_path, sizeof(prompt_path),"/bin/%s", prompt);
 }
