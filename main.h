9fd5eaca0cdf0a0f433d340341c00fe814de5d9c
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-03 12:43:08 -0500

    Fix snprinter function name

--- main.h
+++ main.h
@@ -12,11 +12,7 @@
 void prompter(void);
 void printer(const char *msg);
 char **input_text(char *prompt, size_t extent);
-void snprinter(char *str, size_t extent, const char *format, ...);
-<<<<<<< HEAD
+void snfprinter(char *str, size_t extent, const char *format, ...);
 void executor(const **char arrstore);
-=======
-void executor(char *const arrstore);
->>>>>>> d29b64f74390f385b36b3fe777f221e0fb1f228b
 
 #endif /*MAIN_H_*/
