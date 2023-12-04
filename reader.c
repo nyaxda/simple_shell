ea0609fda210bf532df869debaabe9989ca7ab69
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-04 10:37:26 -0500

    Added space as delimiter in strtok function.

--- reader.c
+++ reader.c
@@ -29,7 +29,7 @@ char **input_text(char *prompt, size_t extent)
 			return (NULL);
 		}
 	}
-	placeholder = strtok(prompt, "\t\n");
+	placeholder = strtok(prompt, " \t\n");
 	arrstore = malloc(sizeof(char *) * 1024);
 	if (arrstore == NULL)
 	{
@@ -39,7 +39,7 @@ char **input_text(char *prompt, size_t extent)
 	while (placeholder)
 	{
 		arrstore[i] = placeholder;
-		placeholder = strtok(NULL, "\t\n");
+		placeholder = strtok(NULL, " \t\n");
 		i++;
 	}
 	arrstore[i] = NULL;
