353be725c687e73333e9589c0fd4f2bb8fa8a608
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-04 07:45:33 -0500

    Fix function parameter type in executor

--- main.h
+++ main.h
@@ -13,6 +13,6 @@ void prompter(void);
 void printer(const char *msg);
 char **input_text(char *prompt, size_t extent);
 void snfprinter(char *str, size_t extent, const char *format, ...);
-void executor(const **char arrstore);
+void executor(const char **arrstore);
 
 #endif /*MAIN_H_*/
