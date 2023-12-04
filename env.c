04110e4b0574c9c00f1b1c07592a6b796aaa27d9
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-04 17:06:47 -0500

    Fix environment variable handling

--- env.c
+++ env.c
@@ -10,8 +10,9 @@ char *_getenviron(const char *current)
     extern char **environ;
     char *value;
     char *name;
-    char *output = NULL, *temp;
+    char *output = NULL, *temp, buffer[1024], temp_buffer[1024], ooutput;
     int i = 0;
+    size_t size, temp_size;
 
     while(environ [i])
     {
@@ -21,16 +22,23 @@ char *_getenviron(const char *current)
         {
             if (name)
             {
-                output = snfprinter("%s", name)
+                size = sizeof(buffer);
+                snfprinter(buffer, size, "%s\n", name);
+                output = strdup(buffer);
                 if (value)
                 {
-                    temp = snfprinter("=%s\n", holder);
+                    temp_size = sizeof(temp_buffer);
+                    snfprinter(temp_buffer, temp_size, "%s\n", value);
+                    temp = strdup(temp_buffer);
                 }
                 else
                 {
-                    temp = snfprinter("=\n");
+                    temp = strdup("=\n");
                 }
+                ooutput = output;
                 output = str_concat(output, temp);
+                free(old_output);
+                free(temp);
             }
         }
         i++;
