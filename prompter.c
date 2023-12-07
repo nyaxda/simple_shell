0be9e6d2ba5ab02a10c703af4e2cf1c0d87b9097
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-07 12:16:37 -0500

    Update prompter function to reflect w directory.

--- prompter.c
+++ prompter.c
@@ -11,6 +11,22 @@ void prompter(void)
 	/*checks if a stdin is a terminal*/
 	if (isatty(STDIN_FILENO))
 	{
-		printer("($) ");
+		char *buffer = malloc(sizeof(char) * 1000), prompt[1024];
+		if (buffer == NULL)
+		{
+			perror("malloc");
+			exit(1);
+		}
+		if (getcwd(buffer, 1000) != NULL)
+		{
+			snfprinter(prompt, sizeof(prompt), "($)%s", buffer);
+			printer(prompt);
+			free(buffer);
+		}
+		else
+		{
+			free(buffer);
+			printer("($) ");
+		}
 	}
 }
