189964b9e0e6f3fa2e68936a77d41369dc5f5736
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-08 17:28:00 -0500

    Fix variable declaration and usage in executor function

--- executor.c
+++ executor.c
@@ -9,10 +9,10 @@
 void executor(const char **arrstore)
 {
 	int status, exitstus;
-	size_t i, j, k, buf_size;
+	size_t i, j, buf_size;
 	pid_t child_process_id = fork();
 	char prompt_path[1024], *en_output, buffer[1024], *output, numbuff[100],
-	*store[1024] = {NULL},	*exit_code[3] = {"echo", NULL, NULL};
+	*exit_code[3] = {"echo", NULL, NULL};
 	const char *directories[] = {"/bin", "/usr/bin", "/usr/sbin", "/sbin"};
 
 	if (strcmp(arrstore[0], "exit") == 0)
@@ -33,9 +33,6 @@ void executor(const char **arrstore)
 	}
 	else if (child_process_id == 0)
 	{
-		for (k = 0; arrstore[k] != NULL; k++)
-			store[k] = (char *)arrstore[k + 1];
-		store[k] = NULL;
 		if (strcmp(arrstore[0], "echo") == 0)
 		{
     		for (j = 1; arrstore[j] != NULL; j++)
@@ -64,7 +61,7 @@ void executor(const char **arrstore)
     	{
         	/* If the command starts with '/', assume it's an absolute path */
         	snprintf(prompt_path, sizeof(prompt_path), "%s", arrstore[0]);
-        	if (execve(prompt_path, (char * const *)store, NULL) == -1)
+        	if (execve(prompt_path, (char * const *)arrstore, NULL) == -1)
 			{
 				perror("Error");
 				exit(1);
@@ -79,7 +76,7 @@ void executor(const char **arrstore)
 				directories[i]);
 				snfprinter(prompt_path + strlen(prompt_path),
 				sizeof(prompt_path)- strlen(prompt_path), "%s", arrstore[0]);
-				if (execve(prompt_path, (char * const *)store, NULL) != -1)
+				if (execve(prompt_path, (char * const *)arrstore, NULL) != -1)
 				{
 					break;
 				}
