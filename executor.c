c9348f3c87bbef30ca07568c11a9562ba7a7cc5c
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-12 08:51:58 -0500

    Refactor executor function

--- executor.c
+++ executor.c
@@ -1,109 +1,86 @@
-#include "main.h"
-
-/**
- * executor -executes a command by forking a child process
- * and using execve to run the command.
- * @arrstore: The array of command arguments to be executed.
- * Return: void
- */
 void executor(const char **arrstore)
 {
-	int status;
-	size_t i, j, buf_size;
+	char **directories, *buffer, *env_output, *numbuff, *path, prompt_path[1024],
+	*token; *exit_code[2];
+	int i, j, status;
 	pid_t child_process_id;
-	char prompt_path[1024], *en_output, buffer[1024], *output,
-	numbuff[100], **directories, *token, *path, *exit_code[2];
 
-	directories = malloc(sizeof(char*) * 1024);
+	directories = malloc(sizeof(char *) * 1024);
 	if (directories == NULL)
 	{
-		perror("Error");
-		exit(1);
+		perror ("Error");
+		return;
 	}
-	path = strdup(getenv("PATH"));
+	path = strdup(getenv("PATH");
 	token = cust_strtk(path, ":");
-	i = 0;
-	while (token != NULL)
+	for (i = 0; token != NULL; i++)
 	{
-    	directories[i] = strdup(token);
-    	token = cust_strtk(NULL, ":");
-		i++;
+		directories[i] = strdup(token);
+		token = cust_strtk(NULL, ":");
 	}
 	directories[i] = NULL;
-	free(path);
-
+	
 	if (strcmp(arrstore[0], "exit") == 0)
 		exit(0);
 	else if (strcmp(arrstore[0], "echo") == 0)
 	{
-    	for (j = 1; arrstore[j] != NULL; j++)
+		for (j = 1; arrstore[j] != NULL; j++)
     	{
         	if (strcmp(arrstore[j], "$?") == 0)
         	{
         		print_integer(WEXITSTATUS(status), numbuff);
-				exit_code[1] = numbuff;
-				execve("/bin/echo", (char * const *)exit_code, NULL);
-    			return;
+				break;
     		}
-    	}
+		}
+		printer(numbuff);
 	}
 	else if (strcmp(arrstore[0], "env") == 0)
 	{
-		buf_size = sizeof(buffer);
-		en_output = _getenviron(NULL);
-		snfprinter(buffer, buf_size, "%s\n", en_output);
-		output = strdup(buffer);
-		printer(output);
-		charfree(en_output);
-		charfree(output);
+		env_output = _getenviron(NULL);
+		buffer = malloc(sizeof(char) * 1024);
+		if (buffer == NULL)
+		{
+			perror("Error: Mem allocation failed");
+			exit(1);
+		}
+		snprintf(buffer, 1024, "%s\n", env_output);
+		printer(buffer);
+		free(buffer);
 	}
-	/*child process occurs here*/
-	i = 0;
-	while (directories[i] != NULL)
+	for (i = 0; directories[i] != NULL; i++)
 	{
-		prompt_path[0] = '\0';
-	
 		if (arrstore[0][0] == '/')
-		{
-    		snprintf(prompt_path, sizeof(prompt_path), "%s", arrstore[0]);
-			/*printf("%s\n", prompt_path);*/
-		}
- 		else
-		{
-			snprintf(prompt_path, sizeof(prompt_path), "%s/%s", directories[i], arrstore[0]);
-			/*printf("%s\n", prompt_path);*/
-		}
-		if (access(prompt_path, F_OK) == 0)
-			break;
+			snprintf(prompt_path, sizeof(prompt_path), "%s", arrstore[0]);
 		else
-			prompt_path[0] = '\0';
-		i++;
+			snprintf(prompt_path, sizeof(prompt_path), "%s/%s",directories[i], arrstore[0]);
+		if (access(prompt_path, F_OK) == 0)
+		{
+			child_process_id = fork();
+			break;
+		}
+		else
+		{
+			perror("Error: Command not found");
+			exit(1);
+		}
 	}
-	if (directories[i] == NULL && prompt_path[0] == '\0')
+	if (child_process_id == -1)
 	{
-		perror("Error");
-		exit(1);
+		perror("Fork Error");
+		exit(0);
+	}
+	else if (child_process_id == 0)
+	{
+		if(execve(prompt_path, (char * const *)arrstore, NULL) < 0)
+		{
+			perror("Error: Execution of command failed");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
-	{
-		child_process_id = fork();
-		/* child process has failed to initiate*/
-		if (child_process_id == -1)
-		{
-			perror("Error");
-			return;
-		}
-		else if (child_process_id == 0)
-		{
-			/*printf("%s before execution\n", prompt_path);*/
-			if (execve(prompt_path, (char * const *)arrstore, NULL) < 0)
-			{
-				perror("Error");
-				return;
-			}
-		}
-		else
-			wait(&status);
-	}
+		wait(&status);
+	for (i = 0; directories[i] != NULL; i++)
+		free(directories[i];
 	free(directories);
+	free(path);
 }
