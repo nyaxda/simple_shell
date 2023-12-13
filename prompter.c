c6e31be9c8b8d7ba457d813946921dd82715e17c
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-13 13:16:26 -0500

    betty-style prompter.c

--- prompter.c
+++ prompter.c
@@ -2,8 +2,7 @@
 
 /**
  * prompter - displays the prompt symbol ($) to the user.
- * Description: prints the prompt symbol, indicating that the user needs
- * to enter a command in the shell.
+ *
  * Return: void.
  */
 void prompter(void)
