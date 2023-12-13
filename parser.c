b2a598a7846fa70635ec4a9291418eeff31ce87f
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-13 02:09:18 -0500

    Refactor parser function to improve readability and fix memory leaks

--- parser.c
+++ parser.c
@@ -2,51 +2,33 @@
 
 char *parser(char **arrstore)
 {
-    char **directories, *path, *token, *prompt_path;
+    char *path, *token = NULL, *prompt_path = NULL, *path_dup;
     int i, found = 0;
 
-    directories = malloc(sizeof(char *) * 1024);
-	if (directories == NULL)
-	{
-		perror ("Mamory allocation error");
-		return (NULL);
-	}
-	path = strdup(getenv("PATH"));
+    if(*arrstore == NULL || arrstore == NULL)
+        return (NULL);
+	path = getenv("PATH");
 	if (path == NULL)
 	{
 		perror("Error: path not found");
-        free(directories);
 		return (NULL);
 	}
-	token = cust_strtk(path, ":");
-	for (i = 0; token != NULL; i++)
-	{
-		directories[i] = strdup(token);
-        if (directories[i] == NULL)
-        {
-            perror("Memory allocation error");
-            free(path);
-            for (i = 0; directories[i] != NULL; i++)
-                free(directories[i]);
-            free(directories);
-            return (NULL);
-        }
-		token = cust_strtk(NULL, ":");
-	}
-	directories[i] = NULL;
-
-	prompt_path = malloc(sizeof(char) * 1024);
-    if (prompt_path == NULL)
+    path_dup = strdup(path);
+    if (path_dup == NULL)
     {
-        perror("Memory allocation error");
-        free(path);
-        for (i = 0; directories[i] != NULL; i++)
-            free(directories[i]);
-        free(directories);
+        perror("strdup");
         return (NULL);
     }
-	for (i = 0; directories[i] != NULL; i++)
+	token = strtok(path_dup, ":");
+	while (token != NULL)
 	{
+        prompt_path = realloc(sizeof(char) * 1024);
+        if (prompt_path == NULL)
+        {
+            perror("Memory reallocation error");
+            free(path_dup);
+            return (NULL);
+        }
 		if (arrstore[0][0] == '/')
 		{
 			snprintf(prompt_path, 1024, "%s", arrstore[0]);
@@ -54,25 +36,23 @@ char *parser(char **arrstore)
 		}
 		else
 		{
-			snprintf(prompt_path, 1024, "%s/%s",directories[i], arrstore[0]);
+			snprintf(prompt_path, 1024, "%s/%s", token, arrstore[0]);
 			printf("%s\n", prompt_path);
 		}
-		if (access(prompt_path, F_OK) == 0)
+		if (access(prompt_path, X_OK) == 0)
 		{
 			printf("%s this is the found path to executable file\n", prompt_path);
 			found = 1;
 			break;
 		}
+        free(prompt_path);
+        prompt_path = NULL;
+        token = strtok(NULL, ":");
 	}
-    free(path);
-    for (i = 0; directories[i] != NULL; i++)
-        free(directories[i]);
-    free(directories);
-    if(found)
-        return (prompt_path);
-    else
-    {
+    free(path_dup);
+    if (!found)
         free(prompt_path);
         return (NULL);
-    }
+
+    return (prompt_path);
 }
