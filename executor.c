b346c95fba7a31dba0fa37a4fb4181121909ac5d
Daniel Nyakundi <nyaxda@gmail.com>
2023-12-13 10:55:47 -0500

    Fix error handling in executor function

--- executor.c
+++ executor.c
@@ -13,7 +13,7 @@ void executor(const char **arrstore)
 	size_t i, j, buf_size;
 	pid_t child_process_id = fork();
 	char prompt_path[1024], *en_output, buffer[1024], *output, numbuff[100],
-	*exit_code[3] = {"echo", NULL, NULL}, *strlteral;
+	*exit_code[3] = {"echo", NULL, NULL};
 	const char *directories[] = {"/bin", "/usr/bin", "/usr/sbin", "/sbin"};
 
 	if (strcmp(arrstore[0], "exit") == 0)
@@ -29,7 +29,7 @@ void executor(const char **arrstore)
 	/* child process has failed to initiate*/
 	if (child_process_id == -1)
 	{
-		perror("2");
+		perror("Error");
 		exit(1);
 	}
 	else if (child_process_id == 0)
@@ -64,7 +64,7 @@ void executor(const char **arrstore)
         	snprintf(prompt_path, sizeof(prompt_path), "%s", arrstore[0]);
         	if (execve(prompt_path, (char * const *)arrstore, environ) == -1)
 			{
-				perror("1");
+				perror("Error");
 				free(arrstore);
 				arrstore = NULL;
 				exit(1);
@@ -87,25 +87,10 @@ void executor(const char **arrstore)
 		}
 		if (i == sizeof(directories)/sizeof(directories[0]))
 		{
-			 if (arrstore[0][0] == '"') 
-    		{
-       			strlteral = strdup(arrstore[0] + 1);
-        		if (strlteral[strlen(strlteral) - 1] == '"') 
-        		{
-            		strlteral[strlen(strlteral) - 1] = '\0';
-        		}
-				fprintf(stderr, "./hsh: %d: %s: not found\n", EXIT_FAILURE, strlteral);
-				free(arrstore);
-				free(strlteral);
-				exit(1);
-    		}
-			else
-			{
-				fprintf(stderr, "./hsh: %d: %s: not found\n", EXIT_FAILURE, arrstore[0]);
-				/*free arrstore memory and exit the child process*/
-				free(arrstore);
-				exit(1);
-			}
+			perror("Error");
+			/*free arrstore memory and exit the child process*/
+			free(arrstore);
+			exit(1);
 		}
 	}
 	else
@@ -115,5 +100,4 @@ void executor(const char **arrstore)
 			exitstus = WEXITSTATUS(status);
 			exit(exitstus);
 	}
-		/*snfprinter(prompt_path, sizeof(prompt_path), "/bin/%s", arrstore[0]);*/
 }
